f3b41cec39997a8e984126e43fff6a5a2dd80611
Tim <73599525+timtogan@users.noreply.github.com>
2026-02-13 16:39:47 -0800

    fixed dt

--- src/main/java/org/carlmontrobotics/subsystems/Drivetrain.java
+++ src/main/java/org/carlmontrobotics/subsystems/Drivetrain.java
@@ -70,16 +70,14 @@ import com.pathplanner.lib.config.RobotConfig;
 import com.revrobotics.spark.ClosedLoopSlot;
 import com.revrobotics.spark.SparkBase;
 import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkFlex;
 import com.revrobotics.spark.SparkMax;
 import com.revrobotics.spark.SparkBase.ControlType;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
+import com.revrobotics.PersistMode;
+import com.revrobotics.ResetMode;
 import com.revrobotics.spark.SparkLowLevel.MotorType;
 import com.revrobotics.spark.config.SparkBaseConfig;
 import com.revrobotics.spark.config.SparkMaxConfig;
 import com.revrobotics.spark.config.ClosedLoopConfig;
-import com.revrobotics.spark.config.ClosedLoopConfig.FeedbackSensor;
 import com.revrobotics.spark.config.SparkBaseConfig.IdleMode;
 
 //units
@@ -405,12 +403,12 @@ public class Drivetrain extends SubsystemBase {
         for (SparkMax turnMotor : turnMotors) {
             SparkMaxConfig config = new SparkMaxConfig();
             config.idleMode(mode);
-            turnMotor.configure(config, SparkBase.ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);      
+            turnMotor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);      
         }
         for (SparkMax driveMotor : driveMotors) {
             SparkMaxConfig config = new SparkMaxConfig();
             config.idleMode(mode);
-            driveMotor.configure(config, SparkBase.ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);     
+            driveMotor.configure(config, ResetMode.kNoResetSafeParameters, PersistMode.kPersistParameters);     
         }
     }
 
