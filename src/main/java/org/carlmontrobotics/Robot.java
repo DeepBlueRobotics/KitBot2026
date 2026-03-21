149be1d68d8ca4c8db6b2cf87ae3e5bb982af8a0
Tim <73599525+timtogan@users.noreply.github.com>
2026-03-21 06:39:05 -0700

    added basic logging

--- src/main/java/org/carlmontrobotics/Robot.java
+++ src/main/java/org/carlmontrobotics/Robot.java
@@ -4,6 +4,9 @@
 
 package org.carlmontrobotics;
 
+import edu.wpi.first.epilogue.Logged;
+import edu.wpi.first.wpilibj.DataLogManager;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -13,6 +16,7 @@ import edu.wpi.first.wpilibj2.command.CommandScheduler;
  * the TimedRobot documentation. If you change the name of this class or the package after creating
  * this project, you must also update the Main.java file in the project.
  */
+@Logged
 public class Robot extends TimedRobot {
   private Command m_autonomousCommand;
 
@@ -25,6 +29,10 @@ public class Robot extends TimedRobot {
   public Robot() {
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
+    // Starts recording to data log
+    DataLogManager.start();
+    // Record both DS control and joystick data
+    DriverStation.startDataLog(DataLogManager.getLog());   
     m_robotContainer = new RobotContainer();
   }
 
